0d2a86205553b8e45a393fe22189619e252746ad
MarceloLeao <leao.marcelo1@hotmail.com>
2019-04-04 18:20:46 -0300

    exercicios

--- array/filter1.js
+++ array/filter1.js
@@ -1,1 +1,14 @@
-ddd
+const produtos = [
+    {nome:'notebook',preco: 2500,fragil: true},
+    {nome:'celular',preco: 1500,fragil: true},
+    {nome:'faca',preco: 30,fragil: false},
+    {nome:'bola',preco: 15,fragil: true}
+]
+
+const caro = p => p.preco > 1000
+const fragil = p => p.fragil
+const nome = p => p.nome
+const preco = p => p.preco
+
+
+console.log(produtos.filter(caro).filter(fragil))
